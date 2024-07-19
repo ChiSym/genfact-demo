fa1940c8cc79546c681760ee5c44dad8a5841553
Ian Limarta <ian.limarta@gmail.com>
2024-07-19 12:22:19 -0500

    Added Project.toml

--- app.jl
+++ app.jl
@@ -1,6 +1,6 @@
 using Oxygen
 using HTTP
-using JSON
+using JSON3
 
 @post "/form" function(req)
     return "this"
@@ -12,17 +12,16 @@ end
 
 
 
-PROMPT = ""
 N_PARTICLES = 5
 GRAMMAR = """ start: "Sequential Monte Carlo is " ("good" | "bad") """
 URL = "http://34.122.30.137:8888/infer"
 
-@post "/sentence-to-pclean" function(req)
-    data = json(req, ToPCleanSentence)
+@post "/sentence-to-pclean" function(request)
+    data = JSON3.read(request.body)
     sentence = data.sentence
 
-    data = Dict(
-        "prompt" => PROMPT, # add sentence to prompt
+    genparse_params = Dict(
+        "prompt" => "", # add sentence to prompt
         "method" => "smc-standard",
         "n_particles" => N_PARTICLES,
         "lark_grammar" => GRAMMAR,
@@ -31,20 +30,15 @@ URL = "http://34.122.30.137:8888/infer"
         "max_tokens" => 100,
         "temperature" => 1.
     )
-    # Convert the dictionary to JSON
-    json_data = JSON.json(data)
-    # Send the POST request
+    json_data = JSON3.write(genparse_params)
+
     response = HTTP.post(URL, ["Content-Type" => "application/json"], json_data)
+    response = JSON3.read(response.body)
 
-    posterior = JSON.parse(String(response.body))["posterior"]
-
-    # post process posterior
-
-
+    response.posterior
 end
 
-@post "/run-pclean" function(req)
-
+@post "/run-pclean" function(request)
 end
 
 serve(port=8888, host="0.0.0.0")
