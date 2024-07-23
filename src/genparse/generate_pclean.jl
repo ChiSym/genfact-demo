ea7589a8a3a4e5de7f461b62c8a3234cd1e297c1
Joe Cecil <jcecil@isi.edu>
2024-07-23 09:45:21 -0400

    Add JSON grammar and prompt template

--- src/genparse/generate_pclean.jl
+++ src/genparse/generate_pclean.jl
@@ -3,12 +3,13 @@ const JSON = "json"
 const NEWLINE = "\n"
 
 # Thanks to https://en.wikibooks.org/wiki/Introducing_Julia/Working_with_text_files
-const GRAMMAR = open("$(@__DIR__)/../resources/pclean_grammar.lark") do file
+const GRAMMAR = open("$(@__DIR__)/../resources/json_grammar.lark") do file
     read(file, String)
 end
 # why specify from_file=false? as in this example: https://docs.juliahub.com/Oxygen/JtS3f/1.5.12/#Mustache-Templating
-format_pclean_prompt = mustache_("$(@__DIR__)/../resources/templates/pclean_prompt_template.txt", from_file=true) 
-format_pclean_code = mustache_("$(@__DIR__)/../resources/templates/pclean_template.jl")
+# maybe because it actually calls a method that may be deprecated in the future:
+# https://docs.juliahub.com/General/Mustache/stable/#Mustache.render_from_file-Tuple{Any,%20Any}
+format_json_prompt = mustache_("$(@__DIR__)/../resources/templates/json_prompt_template.txt", from_file=false)
 
 
 @doc """Extract code from the code block in a chatty Genparse generation."""
