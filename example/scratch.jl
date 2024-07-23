f8b685889280bb6b6b020a768b20eaaeaf8d5117
Ian Limarta <ian.limarta@gmail.com>
2024-07-23 12:22:18 -0500

    Example demo

---
+++ example/scratch.jl
@@ -0,0 +1,57 @@
+using HTTP
+using JSON3
+
+
+# curl "${host:?}"/sentence-to-doctor-data --request POST --header "Content-Type: application/json" --data '{"sentence": "John Smith'\''s neurology office (Happy Brain Services LLC) at 512 Example Street Suite 3600 (CA-170) is terrible!"}'
+
+host = "http://localhost:8888"
+function endpoint1(sentence)
+    body = Dict(
+        "sentence" => sentence
+    )
+    body = JSON3.write(body)
+    
+    url = "$host/sentence-to-doctor-data"
+    response = HTTP.post(url, ["Content-Type"=>"application/json"], body)
+    response = JSON3.read(response.body)
+end
+
+function postprocess(response)
+    result = response[:posterior]
+    k = rand(keys(response[:posterior]))
+    pclean_request = result[k][:as_object]
+    # display(pclean_request)
+    pclean_request = Dict(
+        "observations" => Dict(
+            "first"=> pclean_request["first_name"],
+            "last" => pclean_request["last_name"],
+            # "specialty" => pclean_request["specialty"],
+            "addr" => pclean_request["addr"],
+            "legal_name" => pclean_request["legal_name"]
+        )
+    )
+end
+
+function endpoint2(request)
+    pclean_request = JSON3.write(request)
+    url = "$host/run_pclean"
+    response = HTTP.get(url, ["Content-Type"=>"application/json"], pclean_request)
+    JSON3.read(response.body)
+end
+
+sentence_1 = "Steven Gilman's diagnostic radiology office (Spirit Physician Services Inc) at 429 N 21ST St (CA-170) is terrible!" 
+sentence_2 = "Steven Gilman's diagnostic radiology office (Spirit Physician Services Inc) at 429 N 21ST St (CA-170) is terrible!" 
+
+# 429 N 21ST ST
+# response1 = endpoint1(sentence)
+# pclean_request = postprocess(response1)
+# response2 = endpoint2(pclean_request)
+
+include("viz.jl")
+
+physicians, businesses = group(response2["results"])
+b_df, b_highlights, b_freq = create_tables(businesses, response2["business_histogram"], ["legal_name", "addr", "addr2", "city", "zip"])
+pretty_table(
+    b_df;
+    highlighters= Tuple(b_highlights)
+)
---
+++ example/viz.jl
@@ -0,0 +1,45 @@
+using DataFrames
+using PrettyTables
+using Serialization
+
+function group(results)
+    physicians = Dict{String, Any}()
+    businesses = Dict{String, Any}()
+    for ((p_id, b_id), p_entity, b_entity) in results
+        # println(p_id)
+        # println(b_id)
+        # println(entity)
+        if !(p_id in keys(physicians))
+            physicians[p_id] = []
+        end
+        push!(physicians[p_id], p_entity)
+
+        if !(b_id in keys(businesses))
+            businesses[b_id] = []
+        end
+        push!(businesses[b_id], b_entity)
+    end
+    physicians = Dict(Symbol(key)=>first(val) for (key,val) in physicians)
+    businesses = Dict(Symbol(key)=>first(val) for (key,val) in businesses)
+    physicians, businesses
+end
+
+function create_tables(entities, histogram, attributes)
+    df = DataFrame([[] for _ in attributes], attributes)
+    total = sum(values(histogram))
+
+    freq = Dict(key=>val/total for (key,val) in histogram)
+    entities = sort([(key,val) for (key, val) in entities], by=x->freq[x[1]], rev=true)
+    highlights = []
+
+    interpolate(x) = round(Int,(255-232)*x+232)
+    for (idx,(row_id, entity)) in enumerate(entities)
+        push!(df, entity)
+        f = (data, i, j) -> (i == idx)
+        color = Crayon(background=interpolate(freq[row_id]))
+        push!(highlights, Highlighter(f, color))
+    end
+    freq = sort(collect(values(freq)))
+    df, highlights, freq
+
+end
