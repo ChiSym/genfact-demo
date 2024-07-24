a840c3a7060e865c6a78740179973699192f03f9
Joe Cecil <jcecil@isi.edu>
2024-07-24 18:06:51 +0000

    Initial version of server run script

--- serve.sh
+++ serve.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-julia --project=. src/GenFactDemo.jl
+repo_root=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+if [[ "$(hostname)" = "genfact-server" ]]; then
+  export JULIA_DEPOT_PATH=/srv/julia/.julia
+  export JULIA_LOAD_PATH=/srv/genfactdemo:
+  julia_preamble='DEPOT_PATH[1] = "'"$JULIA_DEPOT_PATH"'"'
+else
+  julia_preammble=""
+fi
+
+#julia_setup='using Pkg; Pkg.activate("'"${repo_root:?}"'"); Pkg.instantiate(); Pkg.add(PackageSpec(url="https://github.com/probcomp/PClean.git", rev="ian/update"))'
+julia_setup=${julia_preamble:+"${julia_preamble}"}'; using Pkg; Pkg.instantiate(); Pkg.add(PackageSpec(url="https://github.com/probcomp/PClean.git", rev="ian/update"))'
+printf '%s\n' "JULIA_DEPOT_PATH is ${JULIA_DEPOT_PATH}"
+export -p | grep JULIA
+sudo -u genfact-demo julia --project="${repo_root:?}" -e "${julia_setup:?}"
+sudo -u genfact-demo julia --project="${repo_root:?}" -e ${julia_preamble:+"${julia_preamble}"}'; include("app.jl")'
