dd74c4e1097a5d42bc540c2f08d6451e5fedfbb1
Joe Cecil <jcecil@isi.edu>
2024-07-25 14:50:57 -0400

    Try to fix nohup logging

--- serve.sh
+++ serve.sh
@@ -17,7 +17,8 @@ if [[ "$(hostname)" = "genfact-server" ]]; then
   sudo touch nohup.out
   sudo chmod o+w nohup.out
   sudo -u genfact-demo julia --project="${repo_root:?}" -e "${julia_setup:?}"
-  > output.log sudo -u genfact-demo nohup julia --project="${repo_root:?}" -e ${julia_preamble:+"${julia_preamble}"}'; include("app.jl")' &
+  cmd=nohup julia --project="${repo_root:?}" -e ${julia_preamble:+"${julia_preamble}"}'; include("app.jl")' > output.log
+  sudo -u genfact-demo sh -c "$cmd" &
 else
   julia --project="${repo_root:?}" -e "${julia_setup:?}"
   julia --project="${repo_root:?}" -e ${julia_preamble:+"${julia_preamble}"}'; include("app.jl")'
