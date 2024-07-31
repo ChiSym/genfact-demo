2ac8815cf712ce1a57e874153f2442be9ca62224
Joe Cecil <jcecil@isi.edu>
2024-07-31 14:40:46 -0400

    Extract annotation logic to function

--- src/genparse/extract_entities.jl
+++ src/genparse/extract_entities.jl
@@ -288,3 +288,64 @@ $(join(labels, "\n"))
 </div>"""
     return result
 end
+
+function get_annotated_sentence_html_posterior(clean_json_posterior, sentence)
+    # Map from keys that we generate using Genparse to the keys that the /run-pclean route expects
+    # jac: This is temporary until we update the grammar/prompt
+    column_names_map =
+        Dict("address" => "addr", "address2" => "addr2", "city" => "city_name", "first_name" => "first", "last_name" => "last")
+
+    result = Dict()
+    for (inference, likelihood) in clean_json_posterior
+        # Post-process the Genparse output to remove empty strings.
+        # Llama3 likes to output empty strings for missing values.
+        # However, empty strings will cause problems for PClean because it will interpret that
+        # to mean "the value of this feature is identically empty" ("this doctor has an empty
+        # string for their specialty").
+        #
+        # We also remove the c2z3 key because the PClean endpoint no longer recognizes this key.
+        #
+        # We could fix these issues in the grammar, however that is out of scope for the August 1st
+        # demo.
+        as_object = Dict{String, String}(
+            String(key) => value for (key, value) in JSON3.read(inference)
+            if !isnothing(value) && strip(value) != "" && strip(uppercase(value)) ∉ _NOTHING_VALUES && key != :c2z3
+                # Llama 3.1 sometimes confabulates values not present in the input sentence.
+                # We don't want to query on those and we don't want to display them in the legend.
+                #
+                # Hack: If the value's not a substring of the sentence, we don't include it in the
+                # object.
+                #
+                # Substring inclusion is not exactly the right thing, because it may discount ways
+                # the information may show up in the sentence indirectly. For example, "I love Dr.
+                # John Smith's office, I can always see the Golden Gate Bridge from his lobby"
+                # implies he's probably practicing in San Francisco or else a very nearby
+                # suburb/town. But it's good enough for now.
+                #
+                # Further Hack: If we don't match a substring of the sentence, we try removing
+                # hyphens from the sentence so that ZIPs are correctly recognized as substrings.
+                # We need to do this because the ZIP codes, as parsed by Llama, shouldn't include
+                # the hyphen.
+                && (!isnothing(findfirst(value, sentence))
+                    || !isnothing(findfirst(value, replace(sentence, "-" => ""))))
+        )
+        # jac: Temporary post-processing step to match the keys that the /run-pclean route expects
+        # jac: Permanent post-processing step to match the value casing used in the Medicare
+        # dataset
+        formatted = Dict(
+            get(column_names_map, key, key) => uppercase(value) for
+            (key, value) in as_object
+        )
+
+        colors = map_attribute_to_color(as_object)
+        legend_entries = [
+            LegendEntry(gloss_attribute(attribute), get_class_name(attribute))
+            for attribute in keys(as_object)
+        ]
+        annotated_text = """$(make_style_tag(colors))
+<p>$(annotate_input_text(sentence, as_object))</p>
+$(make_html_legend(legend_entries))"""
+        result[annotated_text] =
+            Dict("as_object" => formatted, "likelihood" => likelihood)
+    end
+end
