05ce08b8a6b1fde97363282078a6abaf4194e81d
Joe Cecil <jcecil@isi.edu>
2024-08-13 10:13:47 -0400

    Compile chat template at module import
    ... Instead of at compile time, which is useless because then at runtime
    we end up with a null PyObject.

--- src/genparse/chat_template.jl
+++ src/genparse/chat_template.jl
@@ -1,17 +1,18 @@
 const _tokenizer_config_path = "$(@__DIR__)/../../resources/tokenizer_configs/llama3pt1_8b.json"
-const _tokenizer_config = open(_tokenizer_config_path) do file
-    JSON3.read(read(file, String))
-end
-const _template_params = Dict("bos_token" => _tokenizer_config["bos_token"])
-const _chat_template = _tokenizer_config["chat_template"]
-const _jinja2 = pyimport("jinja2")
-const _jinja2sandbox = pyimport("jinja2.sandbox")
-const _jinja2exceptions = pyimport("jinja2.exceptions")
-# jac: I based this code on
-# https://github.com/huggingface/transformers/blob/a24a9a66f446dcb9277e31d16255536c5ce27aa6/src/transformers/tokenization_utils_base.py#L1897
-# but heavily stripped back on the code for simplicity's sake.
-# We don't need to handle automated tool injection or the other complexity Hugging Face supports.
-const _compiled_chat_template = begin
+function __init__()
+    _jinja2 = pyimport("jinja2")
+    _jinja2sandbox = pyimport("jinja2.sandbox")
+    _jinja2exceptions = pyimport("jinja2.exceptions")
+
+    # jac: I based this code on
+    # https://github.com/huggingface/transformers/blob/a24a9a66f446dcb9277e31d16255536c5ce27aa6/src/transformers/tokenization_utils_base.py#L1897
+    # but heavily stripped back on the code for simplicity's sake.
+    # We don't need to handle automated tool injection or the other complexity Hugging Face supports.
+    #
+    # jac: I think  we can avoid copying the whole AssistantTracker code from
+    # https://github.com/huggingface/transformers/blob/v4.43.1/src/transformers/tokenization_utils_base.py#L1921
+    # because it seems to focus on tracking token indices. Because we don't actually need to tokenize the prompt
+    # on our side, this is a moot point.
     py"""
 import json
 
@@ -23,14 +24,17 @@ def raise_exception(message):
 def tojson(x, ensure_ascii=False, indent=None, separators=None, sort_keys=False):
     return json.dumps(x, ensure_ascii=ensure_ascii, indent=indent, separators=separators, sort_keys=sort_keys)
 """
-    # jac: I think  we can avoid copying the whole AssistantTracker code from
-    # https://github.com/huggingface/transformers/blob/v4.43.1/src/transformers/tokenization_utils_base.py#L1921
-    # because it seems to focus on tracking token indices. Because we don't actually need to tokenize the prompt
-    # on our side, this is a moot point.
+
+    _tokenizer_config = open(_tokenizer_config_path) do file
+    JSON3.read(read(file, String))
+end
+    _template_params = Dict("bos_token" => _tokenizer_config["bos_token"])
+    _chat_template = _tokenizer_config["chat_template"]
+
     env = _jinja2sandbox.ImmutableSandboxedEnvironment(trim_blocks=true, lstrip_blocks=true, extensions=[])
     env.filters["tojson"] = py"tojson"
     env.globals["raise_exception"] = py"raise_exception"
-    env.from_string(_chat_template)
+    global _compiled_chat_template = env.from_string(_chat_template)
 end
 
 
