07a6bd280853d1bc3093e506d5dd4fe8f31dff26
Joe Cecil <jcecil@isi.edu>
2024-08-22 10:30:13 -0400

    Write script to generate DocNames sentences

---
+++ scripts/utils/medicare_data.py
@@ -0,0 +1,12 @@
+"""
+Helpers and constants related to the Medicare dataset.
+
+See: https://data.cms.gov/provider-data/dataset/mj5m-pzi6
+"""
+firstname_feature = "Provider First Name"
+lastname_feature = "Provider Last Name"
+specialty_feature = "pri_spec"
+legalname_feature = "Facility Name"
+city_feature = "City/Town"
+zip_feature = "ZIP Code"
+addr_feature = "adr_ln_1"
